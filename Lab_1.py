e02a581e9721ac1ad2a70b3717d4c26c27aeef45
Yelyzaveta Demydenko <yelyzaveta.demydenko@hneu.net>
2025-05-20 21:58:42 +0300

    lab_5

---
+++ Lab_1.py
@@ -0,0 +1,242 @@
+import threading
+
+class House():
+    """Клас житлове приміщення"""
+
+    num = 0
+    def __init__(self, price, availability = True, number = None, owner = None):
+        if number is None:
+            House.num += 1
+        else:
+            House.num = number
+
+        self.__num = House.num
+        self.__price = price
+        self.__availability = availability
+        self.__owner = owner
+
+        self.lock = threading.Lock()
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        self.__price = price
+
+    @property
+    def number(self):
+        return self.__num
+
+    @number.setter
+    def number(self, num):
+        self.__num = num
+
+    @property
+    def owner(self):
+        return self.__owner
+
+    @owner.setter
+    def owner(self, owner):
+        self.__owner = owner
+
+
+    @property
+    def availability(self):
+        return self.__availability
+
+    @availability.setter
+    def availability(self, availability):
+        self.__availability = availability
+
+    def __str__(self):
+        availability_str = "Є в наявності." if self.availability else "Немає в наявності."
+        return f"Житло №{self.number}. Ціна: {self.price}. {availability_str}. Власник: {self.owner}"
+
+class Tenant():
+    """Клас орендатор"""
+
+    def __init__(self, email, name = None, rental_house = None):
+        self.__email = email
+        self.__name = name
+        self.__rental_house = rental_house
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, email):
+        self.__email = email   
+
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name    
+
+    @property
+    def rental_house(self):
+        return self.__rental_house
+
+    @rental_house.setter
+    def rental_house(self, house):
+        self.__rental_house = house
+
+    def rent_house(self, house, landlord, start_date, end_date):
+        return Contract(house, landlord, self, start_date, end_date)
+
+    def __str__(self):
+        string = f"Ім'я орендатора: {self.email}"
+        if self.rental_house:
+            string += f"\nОрендовано житло №{self.rental_house.number}."
+        else:
+            string += "\nНе орендує житло."
+        return string
+
+class Landlord():
+    """Клас орендодавець"""
+    def __init__(self, email, name = None, list_of_houses = None):
+        self.__email = email
+        self.__name = name
+        if list_of_houses is None:
+            self.__list_of_houses = []
+        else:
+            self.__list_of_houses = list_of_houses
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, email):
+        self.__email = email   
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.__name = name    
+
+    @property
+    def list_of_houses(self):
+        return self.__list_of_houses
+
+    def add_house(self, house):
+        self.list_of_houses.append(house)
+        house.owner = self.email
+
+    def add_contract(self, house, landlord, tenant, start_date, end_date):
+        house.lock.acquire()
+        if house.availability:
+            Contract(house, landlord, tenant, start_date, end_date)
+        else:
+            print("Ми не можемо оформити контракт.")
+        house.lock.release()
+
+    def __str__(self):
+        string = f"Ім'я орендодавця: {self.email}"
+
+        if self.list_of_houses:
+            string += "\nСписок житлових приміщень:\n№ Ціна  Наявність"
+            for house in self.list_of_houses:
+                availability_str = "Є в наявності." if house.availability else "Немає в наявності."
+                string += f"\n{house.number} {house.price} {availability_str}"
+        return string
+
+
+class Contract():
+    """Клас договір оренди"""
+    list_of_contracts = []
+    def __init__(self, house, landlord, tenant, start_date, end_date, list_of_contracts = list_of_contracts):
+        self.__house_number = house.number
+        self.__house_price = house.price
+        self.__landlord = landlord
+        self.__tenant = tenant
+        self.__start_date = start_date
+        self.__end_date = end_date
+        list_of_contracts.append(self)
+        
+        house.availability = False
+        tenant.rental_house = house
+
+    @property
+    def house_number(self):
+        return self.__house_number
+            
+    @property
+    def house_price(self):
+        return self.__house_price
+    
+    @property
+    def landlord(self):
+        return self.__landlord
+    
+    @property
+    def tenant(self):
+        return self.__tenant
+    
+    @property
+    def start_date(self):
+        return self.__start_date
+    
+    @property
+    def end_date(self):
+        return self.__end_date
+
+
+    def __str__(self):
+        return (
+            f"Договір оренди житлового приміщення №{self.house_number}"
+            f"\nЦіна оренди в місяць: {self.house_price}"
+            f"\nОрендодавець: {self.landlord.email}"
+            f"\nОрендатор: {self.tenant.email}"
+            f"\nТермін оренди: з {self.start_date} до {self.end_date}"
+            )
+
+if __name__ == "__main__":
+    landlord_Steve = Landlord("Steve")
+    landlord_John = Landlord("John")
+    tenant_Nick = Tenant("Nick")
+    tenant_Fred = Tenant("Fred")
+    print(landlord_Steve)
+    print(landlord_John)
+    print(tenant_Nick)
+    print(tenant_Fred)
+    print()
+
+    house_1 = House(15000)
+    house_2 = House(25000)
+    landlord_Steve.add_house(house_1)
+    landlord_Steve.add_house(house_2)
+    print(landlord_Steve)
+    print(tenant_Nick)
+    print()
+
+
+    t1 = threading.Thread(target = landlord_Steve.add_contract(house_1, landlord_Steve, tenant_Nick, "21.12.2024", "21.05.2025"))
+    t2 = threading.Thread(target = landlord_Steve.add_contract(house_1, landlord_Steve, tenant_Fred, "23.12.2024", "13.05.2025"))
+
+    t1.start()
+    t2.start()
+
+    t1.join()   
+    t2.join()
+
+    print()
+    print(landlord_Steve)
+    print(tenant_Nick)
+    print(tenant_Fred)
+
+    print(house_1)
+    print(house_2)
+
+    print()
+    print(Contract.list_of_contracts[0])
+    print(Contract.list_of_contracts[0].house_price)
---
+++ rental.py
@@ -0,0 +1,398 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
+from starlette.status import HTTP_302_FOUND
+import csv
+
+from Lab_1 import House, Tenant, Landlord, Contract
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+tenants_accounts = dict()
+landlords_accounts = dict()
+tenants = []
+landlords = []
+list_of_houses = []
+
+
+def read_tenants(filename):
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) == 4:
+                if row[3] != "None":
+                    for house in list_of_houses:
+                        if house.number == int(row[3]):
+                            house_ = house
+                            break
+                else:
+                    house_ = None
+                tenants.append(Tenant(email = row[0], name = row[2], rental_house = house_))
+                tenants_accounts[row[0]] = row[1]
+
+
+def read_landlords(filename):
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) == 3:
+                list_of_houses_ = []
+                for house in list_of_houses:
+                    if house.owner == row[0]:
+                        list_of_houses_.append(house)
+                landlords.append(Landlord(email = row[0], name = row[2], list_of_houses = list_of_houses_))
+                landlords_accounts[row[0]] = row[1]
+
+
+def read_houses(filename):
+    with open(filename, newline='', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if len(row) == 4:
+                if row[2] == "True":
+                    availability = True
+                else:
+                    availability = False
+                list_of_houses.append(House(number = int(row[0]), price = row[1], availability = availability, owner = row[3]))
+
+
+def update_landlord():
+    with open("landlords.csv", 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        for landlord in landlords:
+            writer.writerow([landlord.email, landlords_accounts[landlord.email], landlord.name])
+
+
+def update_tenant():
+    with open("tenants.csv", 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        for tenant in tenants:
+            writer.writerow([tenant.email, tenants_accounts[tenant.email], tenant.name, tenant.rental_house.number if tenant.rental_house else "None"])
+
+
+def update_houses():
+    with open("list_of_houses.csv", 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        for house in list_of_houses:
+            writer.writerow([house.number, house.price, house.availability, house.owner])
+        
+
+read_houses("list_of_houses.csv")
+read_landlords("landlords.csv")
+read_tenants("tenants.csv")
+
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/register_tenant")
+def register_tenant_form(request: Request):
+    return templates.TemplateResponse("register_tenant.html", {"request": request})
+
+
+@app.post("/register_tenant")
+def register_tenant(request: Request, email: str = Form(...), name: str = Form(...), password: str = Form(...)):
+    if email == "" or name == "" or password == "":
+        return templates.TemplateResponse("missing_info.html", {"request": request}, status_code=400)
+    if email in tenants_accounts:
+        return templates.TemplateResponse("register_error.html", {"request": request}, status_code=400)
+    tenants_accounts[email] = password
+    tenants.append(Tenant(email, name))
+
+    with open("tenants.csv", 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([email, password, name, "None"])
+
+    return RedirectResponse(url="/", status_code=HTTP_302_FOUND)
+
+
+@app.get("/login_tenant")
+def login_tenant_form(request: Request):
+    return templates.TemplateResponse("login_tenant.html", {"request": request})
+
+
+@app.post("/login_tenant")
+def login_tenant(request: Request, email: str = Form(...), password: str = Form(...)):
+    if email == "" or password == "":
+        return templates.TemplateResponse("missing_info.html", {"request": request}, status_code=400)
+    if tenants_accounts.get(email) == password:
+        response = RedirectResponse(url="/tenant", status_code=HTTP_302_FOUND)
+        response.set_cookie(key="tenant_email", value=email)
+        return response
+    return templates.TemplateResponse("login_error.html", {"request": request}, status_code=400)
+
+
+@app.get("/tenant")
+def tenant(request: Request):
+    return templates.TemplateResponse("tenant.html", {"request": request})
+
+
+@app.get("/tenant_info")
+def tenant_info(request: Request):
+    email = request.cookies.get("tenant_email")
+    for tenant in tenants:
+        if tenant.email == email:
+            tenant_ = tenant
+    if tenant_.rental_house is None:
+        house_ = "Не орендує житло"
+    else:
+        house_ = "Орендує житло №" + str(tenant_.rental_house.number)
+    return templates.TemplateResponse("tenant_info.html", {"request": request, "tenant": tenant_, "house_": house_})
+
+
+@app.get("/tenant_edit_email")
+def tenant_edit_email_form(request: Request):
+    return templates.TemplateResponse("tenant_edit_email.html", {"request": request})
+
+
+@app.post("/tenant_edit_email")
+def tenant_edit_email(request: Request, new_email: str = Form(...)):
+    old_email = request.cookies.get("tenant_email")
+    for tenant in tenants:
+        if tenant.email == old_email:
+            tenant.email = new_email
+            
+            for email in tenants_accounts.keys():
+                if email == old_email:
+                    tenants_accounts[new_email] = tenants_accounts.pop(old_email)
+                    break
+
+
+    update_tenant()
+
+    response = RedirectResponse(url="/tenant_info", status_code=302)
+    response.set_cookie(key="tenant_email", value=new_email)
+    return response
+
+
+@app.get("/tenant_edit_name")
+def tenant_edit_email_form(request: Request):
+    return templates.TemplateResponse("tenant_edit_name.html", {"request": request})
+
+
+@app.post("/tenant_edit_name")
+def tenant_edit_email(request: Request, new_name: str = Form(...)):
+    email = request.cookies.get("tenant_email")
+    for tenant in tenants:
+        if tenant.email == email:
+            tenant.name = new_name
+
+    update_tenant()
+    
+    return RedirectResponse(url="/tenant_info", status_code=302)
+
+
+@app.get("/tenant_houses")
+def tenant_houses(request: Request):
+    return templates.TemplateResponse("tenant_houses.html", {"request": request, "houses": list_of_houses})
+
+
+@app.get("/rent_house/{house_number}")
+def rent_house_form(request: Request, house_number):
+    return templates.TemplateResponse("rent_house.html", {"request": request, "house_number": house_number})
+
+
+@app.post("/rent_house/{house_number}")
+def rent_house(request: Request, house_number: int, start_date: str = Form(...), end_date: str = Form(...)):
+    email = request.cookies.get("tenant_email")
+    for tenant in tenants:
+        if tenant.email == email:
+            tenant_ = tenant
+    for house in list_of_houses:
+        if house.number == house_number:
+            for landlord in landlords:
+                if landlord.email == house.owner:
+                    landlord_ = landlord
+            house_ = house
+
+    contract = tenant_.rent_house(house_, landlord_, start_date, end_date)
+
+    update_tenant()
+    update_houses()
+
+    return templates.TemplateResponse("contract_details.html", {"request": request, "contract": contract})
+
+
+
+
+@app.get("/register_landlord")
+def register_landlord_form(request: Request):
+    return templates.TemplateResponse("register_landlord.html", {"request": request})
+
+
+@app.post("/register_landlord")
+def register_landlord(request: Request, email: str = Form(...), name: str = Form(...), password: str = Form(...)):
+    if email == "" or name == "" or password == "":
+        return templates.TemplateResponse("missing_info.html", {"request": request}, status_code=400)
+    if email in landlords_accounts:
+        return templates.TemplateResponse("register_error.html", {"request": request}, status_code=400)
+    landlords_accounts[email] = password
+    landlords.append(Landlord(email, name))
+
+    with open("landlords.csv", 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([email, password, name])
+
+    return RedirectResponse(url="/", status_code=HTTP_302_FOUND)
+
+
+@app.get("/login_landlord")
+def login_landlord_form(request: Request):
+    return templates.TemplateResponse("login_landlord.html", {"request": request})
+
+
+@app.post("/login_landlord")
+def login_landlord(request: Request, email: str = Form(...), password: str = Form(...)):
+    if email == "" or password == "":
+        return templates.TemplateResponse("missing_info.html", {"request": request}, status_code=400)
+    if landlords_accounts.get(email) == password:
+        response = RedirectResponse(url="/landlord", status_code=HTTP_302_FOUND)
+        response.set_cookie(key="landlord_email", value=email)
+        return response
+    return templates.TemplateResponse("login_error.html", {"request": request}, status_code=400)
+
+
+@app.get("/landlord")
+def landlord(request: Request):
+    return templates.TemplateResponse("landlord.html", {"request": request})
+
+
+@app.get("/landlord_info")
+def landlord_info(request: Request):
+    email = request.cookies.get("landlord_email")
+    for landlord in landlords:
+        if landlord.email == email:
+            landlord_ = landlord
+    if landlord_.list_of_houses:
+        houses = ", ".join("№"+str(house.number) for house in landlord_.list_of_houses)
+    else:
+        houses = "немає"
+        
+    return templates.TemplateResponse("landlord_info.html", {"request": request, "landlord": landlord_, "houses": houses})
+
+
+@app.get("/landlord_edit_email")
+def landlord_edit_email_form(request: Request):
+    return templates.TemplateResponse("landlord_edit_email.html", {"request": request})
+
+
+@app.post("/landlord_edit_email")
+def landlord_edit_email(request: Request, new_email: str = Form(...)):
+    old_email = request.cookies.get("landlord_email")
+    for landlord in landlords:
+        if landlord.email == old_email:
+            landlord.email = new_email
+            landlord_ = landlord
+            
+            
+            for email in landlords_accounts.keys():
+                if email == old_email:
+                    landlords_accounts[new_email] = landlords_accounts.pop(old_email)
+                    break
+    
+    for house in landlord_.list_of_houses:
+        house.owner = landlord_.email
+
+    update_landlord()
+    if landlord_.list_of_houses:
+        update_houses()
+
+    response = RedirectResponse(url="/landlord_info", status_code=302)
+    response.set_cookie(key="landlord_email", value=new_email)
+    return response
+        
+
+@app.get("/landlord_edit_name")
+def landlord_edit_email_form(request: Request):
+    return templates.TemplateResponse("landlord_edit_name.html", {"request": request})
+
+
+@app.post("/landlord_edit_name")
+def landlord_edit_email(request: Request, new_name: str = Form(...)):
+    email = request.cookies.get("landlord_email")
+    for landlord in landlords:
+        if landlord.email == email:
+            landlord.name = new_name
+
+    update_landlord()
+    
+    return RedirectResponse(url="/landlord_info", status_code=302)
+
+
+@app.get("/landlord_houses")
+def landlord_houses(request: Request):
+    email = request.cookies.get("landlord_email")
+    for landlord in landlords:
+        if landlord.email == email:
+            landlord_ = landlord
+            break
+
+    houses_with_tenants = []
+    for house in landlord_.list_of_houses:
+        tenant_ = None
+        for tenant in tenants:
+            if tenant.rental_house == house:
+                tenant_ = tenant
+                break  
+        houses_with_tenants.append((house, tenant_))
+
+    return templates.TemplateResponse("landlord_houses.html", {"request": request, "houses": houses_with_tenants})
+
+
+@app.get("/add_house")
+def add_house_form(request: Request):
+    return templates.TemplateResponse("add_house.html", {"request": request})
+
+
+@app.post("/add_house")
+def add_house_email(request: Request, price: float = Form(...)):
+    email = request.cookies.get("landlord_email")
+    for landlord in landlords:
+        if landlord.email == email:
+            landlord_ = landlord
+    house = House(price)
+    landlord_.add_house(house)
+    list_of_houses.append(house)
+
+    with open("list_of_houses.csv", 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([house.number, house.price, house.availability, house.owner])
+
+    return RedirectResponse(url="/landlord_houses", status_code=302)
+
+
+@app.get("/confirm_delete_house/{house_number}")
+def confirm_delete_house(request: Request, house_number):
+    return templates.TemplateResponse("confirm_delete_house.html", {"request": request, "house_number": house_number})
+
+
+@app.get("/delete_house/{house_number}")
+def delete_house(request: Request, house_number):
+    email = request.cookies.get("landlord_email")
+    for landlord in landlords:
+        if landlord.email == email:
+            landlord_ = landlord
+            break
+
+    for house in list_of_houses:
+        if house.number == int(house_number):
+            house_ = house
+            break
+
+    landlord_.list_of_houses.remove(house_)
+    list_of_houses.remove(house_)
+            
+    update_houses()
+    
+    for tenant in tenants:
+        if tenant.rental_house == house_:
+            tenant.rental_house = None
+            update_tenant()
+            break 
+
+    return RedirectResponse(url="/landlord_houses", status_code=302)
+
